3b1709518f2aa8c9d9209704d9dccf233bd862ce
Sayedgit4 <yasirmiri17@gmail.com>
2021-05-31 13:45:29 -0700

    Verify Confirmation message

--- src/test/java/tests/day1_basicNavigation/FirstClass.java
+++ src/test/java/tests/day1_basicNavigation/FirstClass.java
@@ -1,4 +1,15 @@
 package tests.day1_basicNavigation;
 
+import org.openqa.selenium.WebDriver;
+import utilities.WebDriverFactory;
+
 public class FirstClass {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.close();
+
+    }
 }
